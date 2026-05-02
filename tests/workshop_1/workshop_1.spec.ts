ebbc4c3a72e2ee999699e67ff0465d1fd87d57f4
bilan_daria <131748468+daria-bilan@users.noreply.github.com>
2026-05-02 15:19:59 +0300

    main: workshop 1

---
+++ tests/workshop_1/workshop_1.spec.ts
@@ -0,0 +1,29 @@
+import {Page, test} from  'playwright/test'
+
+test.skip('Basic Navigation', async ({page})=>{
+    await page.goto('https://gitlab.com/');
+    await page.waitForTimeout(3000);
+    await page.reload();
+})
+
+
+test.skip('Interaction with Web Elements on GitLab', async ({page})=>{
+    await page.goto('https://gitlab.com/');
+    const button = page.locator('[data-ga-name="free trial"]');
+    await button.first().click();
+    // await page.locator('[data-testid="new-user-first-name-field"]').fill('John');
+    // await page.locator('[data-testid="new-user-last-name-field"]').fill('Smith');
+    await page.getByTestId('new-user-first-name-field').fill('John');
+    await page.getByTestId('new-user-last-name-field').fill('Smith');
+})
+
+test.only('Using Various Locator Methods', async ({page})=>{
+    await page.setViewportSize({width: 1000, height: 1360});
+    await page.goto('https://gitlab.com/');
+    await page.waitForTimeout(1000);
+    // const menu = page.getByRole('button', {name: 'Main Menu'});
+    // await menu.click();
+    // await page.getByRole('link', {name: 'Sign in'}).click();
+    await page.click(':has-text("Sign in")');
+    await page.waitForTimeout(3000);
+})
