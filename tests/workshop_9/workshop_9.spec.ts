2865bb696750361d37c94ca871073c00e5bc8e6f
bilan_daria <131748468+daria-bilan@users.noreply.github.com>
2026-05-17 16:42:17 +0300

    m: workshop_9, workflows

---
+++ tests/workshop_9/workshop_9.spec.ts
@@ -0,0 +1,29 @@
+import {test, expect} from "@playwright/test"
+
+test.only("Automating Form Submissions @githibAction", async({page})=>{
+    await page.goto('https://demo.playwright.dev/todomvc/')
+
+    const newToDO = page.getByPlaceholder('What needs to be done?')
+    await newToDO.fill('John Doe')
+    await newToDO.press('Enter')
+
+    await newToDO.fill('JJ Doe')
+    await newToDO.press('Enter')
+
+    const firstToDo = page.getByTestId('todo-item').nth(0)
+    await firstToDo.getByRole('checkbox').check()
+    const secondToDo = page.getByTestId('todo-title').nth(1)
+
+    await expect(firstToDo).toHaveClass('completed')
+    await expect(secondToDo).not.toHaveClass('completed')
+})
+
+test.only("Handling Form @githibAction", async({page})=>{
+    await page.goto('https://demo.playwright.dev/todomvc/')
+    await page.fill('[placeholder="What needs to be done?"]', "John Doe")
+    await page.locator('[placeholder="What needs to be done?"]').press('Enter')
+
+    const checkbox = await page.locator('.toggle')
+    await checkbox.check()
+
+})
