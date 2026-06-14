2263807c319e75f7236d6040cd47411a444401f8
bilan_daria <131748468+daria-bilan@users.noreply.github.com>
2026-06-14 16:28:46 +0300

    main: css locator part 1

---
+++ tests/general/css-locator.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, Locator } from '@playwright/test';
+
+test.describe('CSS Locator', () => {
+   test('CSS Locator', async ({ page }) => {
+      // tag and id
+      await page.goto('https://demowebshop.tricentis.com/');
+      const searchBar: Locator = page.locator('input#small-searchterms');
+      await expect(searchBar).toBeVisible();
+      await searchBar.fill('t-Shirts');
+
+      // tag and class
+      await page.locator('input.search-box-text').fill('T-Shirts');
+
+      // tag and attribute
+      await page.locator('input[name=q]').fill('T-Shirts');
+      await page.locator('[name=q]').fill('T-Shirts');
+   });
+});
--- tests/playground/dynamic_table/dynamic_table_test.spec.ts
+++ tests/playground/dynamic_table/dynamic_table_test.spec.ts
@@ -1,31 +1,28 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-test.describe('Dynamic table', ()=>{
-    test.beforeEach(async({page})=>{
-        await page.goto('https://qaplayground.dev/apps/dynamic-table/')
-    })
-    test.skip('Find spider-man real name', async({page})=>{
-        await expect(page.locator('text=SUPERHERO')).toBeVisible()
+test.describe('Dynamic table', () => {
+   test.beforeEach(async ({ page }) => {
+      await page.goto('https://qaplayground.dev/apps/dynamic-table/');
+   });
+   test.skip('Find spider-man real name', async ({ page }) => {
+      await expect(page.locator('text=SUPERHERO')).toBeVisible();
 
-        const expectedRealName = 'Peter Parker'
+      const expectedRealName = 'Peter Parker';
 
-        const row = await page.locator('text="Spider-Man" >> xpath=../../../..')
-        const realName = await row.locator("td").nth(2)
+      const row = await page.locator('text="Spider-Man" >> xpath=../../../..');
+      const realName = await row.locator('td').nth(2);
 
-        await expect(realName).toHaveText(expectedRealName)
+      await expect(realName).toHaveText(expectedRealName);
+   });
 
-    })
+   test.skip('Find Black Widow status', async ({ page }) => {
+      await expect(page.locator('text=SUPERHERO')).toBeVisible();
 
-    test.skip('Find Black Widow status', async({page})=>{
-        await expect(page.locator('text=SUPERHERO')).toBeVisible()
+      const expectedStatus = 'Active';
 
-        const expectedStatus = 'Active'
+      const row = await page.locator('text="Black Widow" >> xpath=../../../..');
+      const currentStatus = await row.locator('td').nth(1);
 
-        const row = await page.locator('text="Black Widow" >> xpath=../../../..')
-        const currentStatus = await row.locator("td").nth(1)
-
-        await expect(currentStatus).toHaveText(expectedStatus)
-    })
-
-})
-
+      await expect(currentStatus).toHaveText(expectedStatus);
+   });
+});
